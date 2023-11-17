2f537a5302ae6a4f2e34a18aaeb6c82b05894d95
Kolade1024 <akoladeemmanuel14@gmail.com>
2023-11-17 22:33:37 +0100

    update

---
+++ public/app.js
@@ -0,0 +1,36 @@
+const iconMenu = document.querySelector("header button");
+
+const menuBar = document.querySelector(".menubar");
+/* menuBar.style.transform = `translateX(${menuBar.offsetWidth})`; */
+
+const menuOpened = document.querySelector(".menubar .menu_opened");
+
+iconMenu.addEventListener("click", (e)=>{
+    menuBar.classList.remove("shift");
+
+   
+});
+
+menuOpened.addEventListener("click",()=>{
+
+   menuBar.classList.add("shift");
+});
+
+
+const mqLarge = window.matchMedia("(min-width: 768px)");
+if (mqLarge.matches) {
+  let Header = document.querySelector(".container header");
+  let nav = document.querySelector("nav");
+  Header.appendChild(nav);
+}
+else{
+    let menuBar = document.querySelector(".menubar");
+  let nav = document.querySelector("nav");
+  menuBar.appendChild(nav);
+}
+
+
+
+
+
+
